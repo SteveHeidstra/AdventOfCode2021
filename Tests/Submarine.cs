0611c60ab6124b5315e395aeba159fd0e23992bd
Steef <heidstra@gmail.com>
2021-12-02 09:08:19 +0100

    Puzzeldag 2, we hebben een onderzeer

---
+++ Tests/Submarine.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class Submarine
+    {
+        public Submarine()
+        {
+            HorizontalPosition = 0;
+            Depth = 0;
+            Aim = 0;
+        }
+
+        public string command
+        {
+            set
+            {
+                string[] x = value.Split(' ');
+
+                switch (x[0])
+                {
+                    case "forward":
+                        Forward = int.Parse(x[1]);
+                        break;
+                    case "up":
+                        Up = int.Parse(x[1]);
+                        break;
+                    case "down":
+                        Down = int.Parse(x[1]);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public int Forward
+        {
+            set
+            {
+                HorizontalPosition += value;
+                Depth += (Aim * value);
+            }
+        }
+
+        public int Up
+        {
+            set
+            {
+                Aim -= value;
+            }
+        }
+        public int Down
+        {
+            set
+            {
+                Aim += value;
+            }
+        }
+
+        public int HorizontalPosition { get; private set; }
+        public int Depth { get; private set; }
+        public int Aim { get; private set; }
+
+
+
+
+    }
+
+}
--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,5 +1,9 @@
 using NUnit.Framework;
 using Kerstpuzzel;
+using System.IO;
+using System.Reflection;
+using System;
+
 namespace Tests
 {
     public class Tests
@@ -7,12 +11,61 @@ namespace Tests
         [SetUp]
         public void Setup()
         {
+            BestandHelper.ApplicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         }
 
         [Test]
-        public void Test1()
+        public void Day2_Puz1E()
         {
-            Assert.Pass();
+            string[] file = BestandHelper.Readfile(@"Input\D2P1E.txt");
+            Submarine sub = new Submarine();
+
+            foreach (string line in file)
+            {
+                Console.WriteLine(line);
+                sub.command = line;
+                Console.WriteLine(sub.HorizontalPosition);
+                Console.WriteLine(sub.Depth);
+
+            }
+
+            Assert.AreEqual(15, sub.HorizontalPosition);
+            Assert.AreEqual(60, sub.Depth);
+        }
+
+        [Test]
+        public void Day2_Puz1()
+        {
+            string[] file = BestandHelper.Readfile(@"Input\D2P1.txt");
+            Submarine sub = new Submarine();
+
+            foreach (string line in file)
+            {
+                sub.command = line;
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine(sub.HorizontalPosition);
+            Console.WriteLine(sub.Depth);
+            Console.WriteLine(sub.HorizontalPosition * sub.Depth);
+        }
+
+        [Test]
+        public void Day2_Puz2()
+        {
+            string[] file = BestandHelper.Readfile(@"Input\D2P1.txt");
+            Submarine sub = new Submarine();
+
+            foreach (string line in file)
+            {
+                sub.command = line;
+               // Console.WriteLine(line);
+            }
+
+            Console.WriteLine(sub.HorizontalPosition);
+            Console.WriteLine(sub.Depth);
+            Console.WriteLine(sub.HorizontalPosition * sub.Depth);
+
         }
     }
 }
