414b316b81f9fde7dd418218d026831dd199e356
Steef <heidstra@gmail.com>
2021-12-07 16:43:32 +0100

    Yo Crabbies, Line up!

---
+++ Tests/CrabSub.cs
@@ -0,0 +1,144 @@
+﻿using Kerstpuzzel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2021
+{
+    [TestFixture]
+    public class CrabSub
+    {
+        [SetUp]
+        public void Setup()
+        {
+            BestandHelper.ApplicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public void CalculatePopulationTest()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D7P1E.txt").First().Split(',').Select(Int32.Parse).ToList();
+            List<int> positions = population.Distinct().ToList();
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+
+            foreach (int pos in positions)
+            {
+                counters.Add(pos, population.Count(x => x == pos));
+            }
+
+            int minfuel = int.MaxValue;
+            int minpos = int.MinValue;
+
+            foreach(int pos in positions)
+            {
+                int fuel = counters.Sum((item) => Math.Abs(item.Key - pos) * item.Value);
+                
+                if(fuel < minfuel)
+                {
+                    minfuel = fuel;
+                    minpos = pos;
+                }
+            }
+
+            Assert.AreEqual(37, minfuel);
+            Assert.AreEqual(2, minpos);
+        }
+
+        [Test]
+        public void Day7Puz1()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D7P1.txt").First().Split(',').Select(Int32.Parse).ToList();
+            List<int> positions = population.Distinct().ToList();
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+
+            foreach (int pos in positions)
+            {
+                counters.Add(pos, population.Count(x => x == pos));
+            }
+
+            int minfuel = int.MaxValue;
+            int minpos = int.MinValue;
+
+            foreach (int pos in positions)
+            {
+                int fuel = counters.Sum((item) => Math.Abs(item.Key - pos) * item.Value);
+
+                if (fuel < minfuel)
+                {
+                    minfuel = fuel;
+                    minpos = pos;
+                }
+            }
+
+            Assert.AreEqual(341534, minfuel);
+            Assert.AreEqual(363, minpos);
+        }
+
+        [Test]
+        public void Day7Puz2E()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D7P1E.txt").First().Split(',').Select(Int32.Parse).ToList();
+            List<int> positions = population.Distinct().ToList();
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+            
+            foreach (int pos in positions)
+            {
+                counters.Add(pos, population.Count(x => x == pos));
+            }
+
+            Int64 minfuel = int.MaxValue;
+            int minpos = int.MinValue;
+
+            for (int pos = 0; pos < positions.Max(); pos++)
+            {
+                Int64 fuel = counters.Sum((item) => (Math.Abs(item.Key - pos) * (Math.Abs(item.Key - pos) + 1)/2) * item.Value);
+                
+                if (fuel < minfuel)
+                {
+                    minfuel = fuel;
+                    minpos = pos;
+                }
+            }
+
+            Assert.AreEqual(168, minfuel);
+            Assert.AreEqual(5, minpos);
+        }
+
+        [Test]
+        public void Day7Puz2()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D7P1.txt").First().Split(',').Select(Int32.Parse).ToList();
+            List<int> positions = population.Distinct().ToList();
+            Dictionary<int, int> counters = new Dictionary<int, int>();
+
+            foreach (int pos in positions)
+            {
+                counters.Add(pos, population.Count(x => x == pos));
+            }
+
+            Int64 minfuel = int.MaxValue;
+            int minpos = int.MinValue;
+
+            for (int pos = 0; pos < positions.Max(); pos++)
+            {
+                Int64 fuel = counters.Sum((item) => (Math.Abs(item.Key - pos) * (Math.Abs(item.Key - pos) + 1) / 2) * item.Value);
+
+                if (fuel < minfuel)
+                {
+                    minfuel = fuel;
+                    minpos = pos;
+                }
+            }
+
+            Assert.AreEqual(93397632, minfuel);
+            Assert.AreEqual(484, minpos);
+        }
+
+    }
+}
---
+++ Tests/LanternFish.cs
@@ -0,0 +1,87 @@
+﻿using Kerstpuzzel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2021
+{
+    [TestFixture]
+    public class LanternFish
+    {
+        [SetUp]
+        public void Setup()
+        {
+            BestandHelper.ApplicationPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+
+        [Test]
+        public void CalculatePopulationTest()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D6P1E.txt").First().Split(',').Select(Int32.Parse).ToList();
+            Assert.AreEqual(26, CalculatePopulation(population, 18));
+            Assert.AreEqual(5934, CalculatePopulation(population, 80));
+        }
+
+        [Test]
+        public void CalculatePopulationP2()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D6P1.txt").First().Split(',').Select(Int32.Parse).ToList();
+           // Assert.AreEqual(365131, CalculatePopulation(population, 80));
+            Assert.AreEqual(365131, CalculatePopulation(population, 256));
+        }
+
+        [Test]
+        public void CalculateBigPopulationP2()
+        {
+            List<int> population = BestandHelper.Readfile(@"Input\D6P1.txt").First().Split(',').Select(Int32.Parse).ToList();
+            Assert.AreEqual(365131, CalculateBigPopulation(population, 256));
+        }
+
+        public int CalculateBigPopulation(List<int> population, int days)
+        {
+            return 0;
+
+        }
+
+        public int CalculatePopulation(List<int> population, int days)
+        {
+            List<int> newPopulation = population.ToList<int>();
+            BestandHelper.SaveObject<List<int>>(newPopulation, @"newPop0" );
+
+            for (int i = 0; i < days; i++)
+            {
+                newPopulation = BestandHelper.LoadObject<List<int>>("newPop" + i);
+                newPopulation = TomorrowsPopulation(newPopulation.ToList<int>());
+                BestandHelper.SaveObject<List<int>>(newPopulation, @"newPop" + (i+1));
+
+                newPopulation = null;
+            }
+
+            newPopulation = BestandHelper.LoadObject<List<int>>(@"newPop" + (days));
+            return newPopulation.Count;
+        }
+
+        public List<int> TomorrowsPopulation(List<int> population)
+        {
+            List<int> newfish = new List<int>();
+
+            int countThe8 = population.Count(x => x == 0);
+            foreach (int fish in population)
+            {
+                int fishIndex = fish - 1;
+                newfish.Add(fishIndex < 0 ? 6 : fishIndex);
+            }
+            for (int j = 0; j < countThe8; j++)
+            {
+                newfish.Add(8);
+            }
+
+            return newfish;
+        }
+    }
+}
